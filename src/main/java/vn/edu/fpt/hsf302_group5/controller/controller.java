570dc6cd05e1012c805578d7ba8863875c4154fe
Thanh Do <dothanh2572005@gmail.com>
2026-06-21 12:16:53 +0700

    add candidate screen

--- src/main/java/vn/edu/fpt/hsf302_group5/controller/controller.java
+++ src/main/java/vn/edu/fpt/hsf302_group5/controller/controller.java
@@ -1,4 +1,13 @@
 package vn.edu.fpt.hsf302_group5.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class controller {
+
+    @GetMapping("/test")
+    public String test(){
+        return "pages/candidate/application-detail";
+    }
 }
