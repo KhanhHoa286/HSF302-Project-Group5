c7b23bed00b4129e9c550057243b4f0882550c44
Thanh Do <dothanh2572005@gmail.com>
2026-07-07 19:47:24 +0700

    abc

---
+++ src/main/java/vn/edu/fpt/hsf302_group5/repository/jobpost/JobPostSpecificationRepository.java
@@ -0,0 +1,7 @@
+package vn.edu.fpt.hsf302_group5.repository.jobpost;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import vn.edu.fpt.hsf302_group5.entity.JobPost;
+
+public interface JobPostSpecificationRepository extends JpaSpecificationExecutor<JobPost> {
+}
